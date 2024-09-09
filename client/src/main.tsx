2b90fdc0904e6182a93e3b7e142e10e1e1738507
Daniel Campos <dev_campos@hotmail.com>
2024-09-09 10:56:22 -0300

    chore: debug vercel

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import Routes from "./routes/Routes.tsx";
+import Routes from "./routes/Routes";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 
